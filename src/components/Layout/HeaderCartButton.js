1a33657443ecb27a47ad40c03d25e9087593fd55
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2021-08-05 10:52:10 +0300

    Implement animation on cart update

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
@@ -6,15 +6,34 @@ import Button from "../UI/Button/Button";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const {items} = cartCtx;
 
   //Outpouts the sum amount of items in the array
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <Button className={classes.button} onClick={props.showCartBtn}>
+    <Button className={btnClasses} onClick={props.showCartBtn}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
